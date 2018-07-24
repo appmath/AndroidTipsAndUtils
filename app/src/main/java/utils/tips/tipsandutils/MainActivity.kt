57f994d904a287105d456fd7612d9d7e785916da
felz <aziz.kadhi.felz@statefarm.com>
2018-07-24 10:29:26 -0700

    First push

---
+++ app/src/main/java/utils/tips/tipsandutils/MainActivity.kt
@@ -0,0 +1,19 @@
+package utils.tips.tipsandutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import utils.tips.tipsandutils.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
